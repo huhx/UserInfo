1ae0e70f8d689643e49cd10debc4c0a9de0370d8
huhx <huhongxiang@csii.com.cn>
2016-09-09 13:00:15 +0800

    git 3

--- UserInfo/src/main/java/com/example/huhx/GitTest.java
+++ UserInfo/src/main/java/com/example/huhx/GitTest.java
@@ -1,8 +1,7 @@
 package com.example.huhx;
 
 public class GitTest {
-
 	public static void main(String[] args) {
-		System.out.println("hello git.");
+		System.out.println("hello git");
 	}
 }
--- UserInfo/src/main/java/com/example/huhx/GitTest2.java
+++
@@ -1,7 +0,0 @@
-package com.example.huhx;
-
-public class GitTest2 {
-	public static void main(String[] args) {
-		System.out.println("hello git2.");
-	}
-}
